2cb50f3091a77dfc271916b3a2ecf2905f1031b4
Luan4560 <luan.nascimento.dev@gmail.com>
2025-04-04 22:00:29 -0300

    chore: revert prompt

--- src/services/horoscopeService.ts
+++ src/services/horoscopeService.ts
@@ -9,22 +9,12 @@ export const getSarcasticHoroscope = async (
   try {
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
-      contents: `
-      Olha só, eu preciso que você me ajude a
-      criar uma descrição do lado sombrio de um signo zodiacal,
-      porque, aparentemente, ${name} aqui, do signo de ${sign},
-      acha que vive numa bolha de perfeição estelar.
-      Por favor, me entregue um texto tão sarcasticamente
-      brilhante que até as estrelas vão revirar os olhos de inveja.
-      Foque nos defeitos mais ridículos e irritantes desse signo,
-      com aquele humor esperto que faz a gente rir enquanto concorda em segredo.
-      Nada de xingamentos, porque ${name} pode ser sensível demais pra lidar com a
-      verdade nua e crua, mas capriche no tom de 'você sabe que é assim mesmo'.
-      Vamos lá, destrua essa ilusão zodiacal com classe!
-      resuma em 2 paragrafos maximo 400 palavras
-      remova linha em branco no final do texto
-      retorne um markdown estruturado resuma nao usar emojis e nao inclua
-      `,
+      contents: `eu preciso que você me ajude a criar uma descrição do lado sombrio de um signo zodiacal, porque,
+       aparentemente, ${name} aqui, do signo de ${sign}, acha que vive numa bolha de perfeição estelar. 
+       Por favor, me entregue um texto tão sarcasticamente brilhante que até as estrelas vão revirar os olhos de inveja. 
+       Foque nos defeitos mais ridículos e irritantes desse signo, com aquele humor esperto que faz a gente rir enquanto concorda em segredo. 
+       Nada de xingamentos, porque ${name} pode ser sensível demais pra lidar com a verdade nua e crua, mas capriche no tom de 'você sabe que é assim mesmo'. 
+       Vamos lá, destrua essa ilusão zodiacal com classe!`,
     });
 
     return response.text;
